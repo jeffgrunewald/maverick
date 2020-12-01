ca03d316811e88583441eb45b65ccf496251246c
jeffgrunewald <jeff@grunewalddesign.com>
2020-12-01 13:23:21 -0500

    ssl config

--- lib/maverick/api/supervisor.ex
+++ lib/maverick/api/supervisor.ex
@@ -12,16 +12,31 @@ defmodule Maverick.Api.Supervisor do
   def init({api, _otp_app, opts} = init_args) do
     port = Keyword.get(opts, :port, 4000)
     handler = Module.concat(api, Handler)
-    # name = Keyword.get(opts, :name, Module.concat(api, Webserver))
+    name =
+      opts
+      |> Keyword.get(:name, Module.concat(api, Webserver))
+      |> format_name()
+
+    standard_config = [port: port, callback: handler, name: name]
+
+    ssl_config =
+      case Keyword.get(opts, :ssl, []) do
+        [certfile: _, keyfile: _] = ssl -> [{:ssl, true} | ssl]
+        _ -> []
+      end
 
     children = [
       {Maverick.Api.Initializer, init_args},
       %{
         id: :elli,
-        start: {:elli, :start_link, [[callback: handler, port: port]]}
+        start: {:elli, :start_link, [Keyword.merge(standard_config, ssl_config)]}
       }
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
   end
+
+  defp format_name({:via, _, _} = name), do: name
+  defp format_name({:global, _} = name), do: name
+  defp format_name(name) when is_atom(name), do: {:local, name}
 end
